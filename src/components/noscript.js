4aa427c20bd4841644702b7f96a2b4cc446ef507
Ajmal Afif Jamal <ajmal.jamal@ITSG002907-MAC.local>
2018-08-24 11:52:39 +0800

    Testing noscript with content this time, but within body

--- src/components/noscript.js
+++ src/components/noscript.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 const Noscript = () => (
     <noscript>
+        <div>
+            <h1>Gatsby Starter. Please turn on your JS for best experience.</h1>
+        </div>
     </noscript>
 )
 
